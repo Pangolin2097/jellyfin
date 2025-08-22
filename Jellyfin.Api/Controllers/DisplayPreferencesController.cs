5165f46d35d3f59390102a717ddbef2762030edd
Florian Gimat <florian.gimat@outlook.com>
2025-08-22 11:58:46 -0700

    Ensure the default HomeSections on the server side match the web side

--- Jellyfin.Api/Controllers/DisplayPreferencesController.cs
+++ Jellyfin.Api/Controllers/DisplayPreferencesController.cs
@@ -127,11 +127,13 @@ public class DisplayPreferencesController : BaseJellyfinApiController
             HomeSectionType.SmallLibraryTiles,
             HomeSectionType.Resume,
             HomeSectionType.ResumeAudio,
-            HomeSectionType.ResumeBook,
-            HomeSectionType.LiveTv,
             HomeSectionType.NextUp,
-            HomeSectionType.LatestMedia,
             HomeSectionType.None,
+            HomeSectionType.None,
+            HomeSectionType.None,
+            HomeSectionType.None,
+            HomeSectionType.None,
+            HomeSectionType.None
         };
 
         if (!Guid.TryParse(displayPreferencesId, out var itemId))
